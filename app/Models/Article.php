7409c28e71eff3b96302f0cdc904bc6a305b8956
Muhammad Haikal Syamsuddin <udingaming0903@gmail.com>
2025-09-28 21:55:23 +0700

    edit article.php and user.php

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Article extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['title', 'content', 'user_id',];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
